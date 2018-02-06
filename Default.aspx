ec73f25f77c3746a658ba070f2f9c1fe5ca86795
Karthik Narayanan <karthikn@ufl.edu>
2018-02-06 13:21:17 -0500

    One and only commit

---
+++ Default.aspx
@@ -0,0 +1,237 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Language Translator</title>
+    <style type="text/css">
+
+        .body{
+            background-image: url('Images/background.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .fullWidth{
+            width: 100%;
+        }
+
+        .halfWidth{
+            width: 50%;
+        }
+
+        .panelSpaceLess{
+            width: 100%;
+            margin: 20px 0px;
+        }
+
+        .panelSpaceMore{
+            width: 100%;
+            margin: 60px 0px;
+        }
+
+        .headingLabel {
+            font-family: 'Trebuchet MS';
+            font-size: 40px;
+            font-weight: normal;
+            color: white;
+            line-height: 200%;
+            display: block;
+            text-align: center;
+        }
+
+        .languageSelectionLabel {
+            font-family: 'Trebuchet MS';
+            font-size: 20px;
+            font-weight: normal;
+            color: white;
+            line-height: 200%;
+            float: right;
+            margin-right: 10px;
+        }
+
+        .languageSelectionDropDown {
+            background-color: rgba(0, 0, 0, 0.3);
+            font-family: 'Trebuchet MS';
+            font-size: 18px;
+            font-weight: normal;
+            color: white;
+            border: 0.5px solid #808080;
+            border-radius: 3px;
+            margin-left: 10px;
+            padding: 5px;
+        }
+
+        .languageSelectionDropDownItem { 
+            color: black;
+        }
+
+        .textTranslationTextBoxFrom {
+            background-color: rgba(0, 0, 0, 0.3);
+            width: 70%;
+            min-width: 250px;
+            min-height: 200px;
+            font-family: 'Trebuchet MS';
+            font-size: 16px;
+            font-weight: normal;
+            color: white;
+            border: none;
+            border-radius: 15px;
+            padding: 12px 12px 0px 12px;
+            float: right;
+            overflow: hidden;
+        }
+
+        translateButtonCell{
+            min-width: 74px;
+            min-height: 74px;
+        }
+
+        .translateButton {
+            vertical-align: central;
+        }
+
+        .translateButton:hover {
+            cursor:pointer;
+        }
+
+        .textTranslationTextBoxTo {
+            background-color: rgba(0, 0, 0, 0.3);
+            width: 70%;
+            min-width: 250px;
+            min-height: 200px;
+            font-family: 'Trebuchet MS';
+            font-size: 16px;
+            font-weight: normal;
+            color: white;
+            border: none;
+            border-radius: 15px;
+            padding: 12px 12px 0px 12px;
+            float: left;
+            overflow: hidden;
+        }
+
+        .errorButton {
+            background-color: #ed1c24; /* Red */
+            font-family: 'Trebuchet MS';
+            font-size: 17px;
+            font-weight: normal;
+            color: white;
+            text-align: center;
+            text-decoration: none;
+            border: none;
+            border-radius: 7px;
+            padding: 15px 32px;
+        }
+
+        .errorButton:hover {
+            cursor:pointer;
+        }
+
+        .completeButton {
+            background-color: #4caf50; /* Green */
+            font-family: 'Trebuchet MS';
+            font-size: 17px;
+            font-weight: normal;
+            color: white;
+            text-align: center;
+            text-decoration: none;
+            border: none;
+            border-radius: 7px;
+            padding: 15px 32px;
+        }
+
+        .completeButton:hover {
+            cursor:pointer;
+        }
+
+        .errorLabel {
+            font-family: 'Trebuchet MS';
+            font-size: 14px;
+            font-weight: normal;
+            color: white;
+            display: block;
+            text-align: center;
+            padding-top: 24px;
+        }
+
+        .errorCountLabel {
+            font-family: 'Trebuchet MS';
+            font-size: 14px;
+            font-weight: normal;
+            color: white;
+            line-height: 200%;
+            display: block;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body class="body">
+    <form id="form1" runat="server">
+
+        <asp:Panel ID="HeadingPanel" runat="server" CssClass="panelSpaceLess">
+            <asp:Table ID="HeadingTable" runat="server" CssClass="fullWidth">
+                <asp:TableRow ID="HeadingTableRow" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="HeadingTableRowCell" runat="server" CssClass="fullWidth">
+                        <asp:Label ID="HeadingLabel" runat="server" CssClass="headingLabel" Text="Language Translator"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:Panel>
+
+        <asp:Panel ID="LanguageSelectionPanel" runat="server" CssClass="panelSpaceLess">
+            <asp:Table ID="LanguageSelectionTable" runat="server" CssClass="fullWidth">
+                <asp:TableRow ID="LanguageSelectionTableRow" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="LanguageSelectionTableRowCell1" runat="server" CssClass="halfWidth">
+                        <asp:Label ID="LanguageSelectionLabel" runat="server" CssClass="languageSelectionLabel" Text="Translate to language:"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell ID="LanguageSelectionTableRowCell2" runat="server" CssClass="halfWidth">
+                        <asp:DropDownList ID="LanguageSelectionDropDownList" runat="server" CssClass="languageSelectionDropDown"></asp:DropDownList>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:Panel>
+
+        <asp:Panel ID="TextTranslationPanel" runat="server" CssClass="panelSpaceMore">
+            <asp:Table ID="TextTranslationTable" runat="server" CssClass="fullWidth">
+                <asp:TableRow ID="TextTranslationTableRow" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="TextTranslationTableRowCell1" runat="server">
+                        <asp:TextBox ID="TextTranslationFrom" runat="server" CssClass="textTranslationTextBoxFrom" TextMode="MultiLine" Placeholder="Enter the text to be translated. (Language is auto-detected)"></asp:TextBox>
+                    </asp:TableCell>
+                    <asp:TableCell ID="TextTranslationTableRowCell2" runat="server" CssClass="translateButtonCell" HorizontalAlign="Center">
+                        <asp:ImageButton ID="TranslateButton" runat="server" CssClass="translateButton" ImageUrl="~/Images/rightArrow_64px.png" ToolTip="Convert" OnClick="TranslateButton_Click" />
+                    </asp:TableCell>
+                    <asp:TableCell ID="TextTranslationTableRowCell3" runat="server">
+                        <asp:TextBox ID="TextTranslationTo" runat="server" CssClass="textTranslationTextBoxTo" TextMode="MultiLine" Placeholder="The translated text will appear here." ReadOnly="true"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:Panel>
+
+        <asp:Panel ID="ButtonPanel" runat="server" CssClass="panelSpaceMore">
+            <asp:Table ID="ButtonTable" runat="server" CssClass="fullWidth">
+                <asp:TableRow ID="ButtonTableRow1" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="ButtonTableRow1Cell1" runat="server" CssClass="fullWidth" HorizontalAlign="Center">
+                        <asp:Button ID="ErrorButton" runat="server" CssClass="errorButton" Text="Error" ToolTip="Error" OnClick="ErrorButton_Click" />
+                        &nbsp; &nbsp;
+                        <asp:Button ID="CompleteButton" runat="server" CssClass="completeButton" Text="Complete Interaction" ToolTip="Complete" OnClick="CompleteButton_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow ID="ButtonTableRow2" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="ButtonTableRow2Cell1" runat="server" CssClass="fullWidth">
+                        <asp:Label ID="ErrorLabel" runat="server" CssClass="errorLabel" Text="Sorry about that!! Please try again rephrasing the input text!!" Visible="false"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow ID="ButtonTableRow3" runat="server" CssClass="fullWidth">
+                    <asp:TableCell ID="ButtonTableRow3Cell1" runat="server" CssClass="fullWidth">
+                        <asp:Label ID="ErrorCountLabel" runat="server" CssClass="errorCountLabel" Text="Error Count: 0" Visible="false"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:Panel>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Media;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    // Before running the application, input the secret key for your subscription to Translator Text Translation API.
+    private const string TEXT_TRANSLATION_API_SUBSCRIPTION_KEY = "b607858a081b4e61a95ae10c2cd0f073";
+
+    // Object to get an authentication token
+    private AzureAuthToken tokenProvider;
+    // Cache language friendly names
+    private string[] friendlyName = { " " };
+    // Cache list of languages for speech synthesis
+    private List<string> speakLanguages;
+    // Dictionary to map language code from friendly name
+    private Dictionary<string, string> languageCodesAndTitles = new Dictionary<string, string>();
+
+    // Error Count
+    private static int errorCount = 0;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        tokenProvider = new AzureAuthToken(TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
+        GetLanguagesForTranslate(); //List of languages that can be translated
+        GetLanguageNamesMethod(tokenProvider.GetAccessToken(), friendlyName); //Friendly name of languages that can be translated
+        GetLanguagesForSpeakMethod(tokenProvider.GetAccessToken()); //List of languages that have a synthetic voice for text to speech
+        enumLanguages(); //Create the drop down list of langauges
+    }
+
+    //*****POPULATE COMBOBOX*****
+    private void enumLanguages()
+    {
+        //run a loop to load the combobox from the dictionary
+        var count = languageCodesAndTitles.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            LanguageSelectionDropDownList.Items.Add(languageCodesAndTitles.ElementAt(i).Key);
+            LanguageSelectionDropDownList.Items[i].Attributes.Add("class", "languageSelectionDropDownItem");
+        }
+    }
+
+    protected void TranslateButton_Click(object sender, System.Web.UI.ImageClickEventArgs e)
+    {
+        ErrorLabel.Visible = false;
+        ErrorCountLabel.Visible = false;
+        string languageCode;
+        languageCodesAndTitles.TryGetValue(LanguageSelectionDropDownList.SelectedItem.Text, out languageCode); //get the language code from the dictionary based on the selection in the combobox
+
+        if (languageCode == null)  //in case no language is selected.
+        {
+            languageCode = "en";
+        }
+
+        //*****BEGIN CODE TO MAKE THE CALL TO THE TRANSLATOR SERVICE TO PERFORM A TRANSLATION FROM THE USER TEXT ENTERED INCLUDES A CALL TO A SPEECH METHOD*****
+
+        string txtToTranslate = TextTranslationFrom.Text;
+
+        string uri = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Translate?text=" + System.Web.HttpUtility.UrlEncode(txtToTranslate) + "&to={0}", languageCode);
+
+        WebRequest translationWebRequest = WebRequest.Create(uri);
+
+        translationWebRequest.Headers.Add("Authorization", tokenProvider.GetAccessToken()); //header value is the "Bearer plus the token from ADM
+
+        WebResponse response = null;
+
+        response = translationWebRequest.GetResponse();
+
+        Stream stream = response.GetResponseStream();
+
+        Encoding encode = Encoding.GetEncoding("utf-8");
+
+        StreamReader translatedStream = new StreamReader(stream, encode);
+
+        System.Xml.XmlDocument xTranslation = new System.Xml.XmlDocument();
+
+        xTranslation.LoadXml(translatedStream.ReadToEnd());
+
+        TextTranslationTo.Text = /*"Translation -->   " +*/ xTranslation.InnerText;
+
+        if (speakLanguages.Contains(languageCode) && txtToTranslate != "")
+        {
+            //call the method to speak the translated text
+            SpeakMethod(tokenProvider.GetAccessToken(), xTranslation.InnerText, languageCode);
+        }
+    }
+
+    //*****SPEECH CODE*****
+    private void SpeakMethod(string authToken, string textToVoice, String languageCode)
+    {
+        string translatedString = textToVoice;
+
+        string uri = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Speak?text={0}&language={1}&format=" + HttpUtility.UrlEncode("audio/wav") + "&options=MaxQuality", translatedString, languageCode);
+
+        WebRequest webRequest = WebRequest.Create(uri);
+        webRequest.Headers.Add("Authorization", authToken);
+        WebResponse response = null;
+        try
+        {
+            response = webRequest.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (SoundPlayer player = new SoundPlayer(stream))
+                {
+                    player.PlaySync();
+                }
+            }
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            if (response != null)
+            {
+                response.Close();
+                response = null;
+            }
+        }
+    }
+
+    //*****CODE TO GET TRANSLATABLE LANGAUGE CODES*****
+    private void GetLanguagesForTranslate()
+    {
+        string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate";
+        WebRequest WebRequest = WebRequest.Create(uri);
+        WebRequest.Headers.Add("Authorization", tokenProvider.GetAccessToken());
+
+        WebResponse response = null;
+
+        try
+        {
+            response = WebRequest.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                List<string> languagesForTranslate = (List<string>)dcs.ReadObject(stream);
+                friendlyName = languagesForTranslate.ToArray(); //put the list of language codes into an array to pass to the method to get the friendly name.
+            }
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            if (response != null)
+            {
+                response.Close();
+                response = null;
+            }
+        }
+    }
+
+    //*****CODE TO GET TRANSLATABLE LANGAUGE FRIENDLY NAMES FROM THE TWO CHARACTER CODES*****
+    private void GetLanguageNamesMethod(string authToken, string[] languageCodes)
+    {
+        string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguageNames?locale=en";
+        // create the request
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.Headers.Add("Authorization", tokenProvider.GetAccessToken());
+        request.ContentType = "text/xml";
+        request.Method = "POST";
+        System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String[]"));
+        using (System.IO.Stream stream = request.GetRequestStream())
+        {
+            dcs.WriteObject(stream, languageCodes);
+        }
+        WebResponse response = null;
+        try
+        {
+            response = request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                string[] languageNames = (string[])dcs.ReadObject(stream);
+
+                for (int i = 0; i < languageNames.Length; i++)
+                {
+                    languageCodesAndTitles.Add(languageNames[i], languageCodes[i]); //load the dictionary for the combo box
+                }
+            }
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            if (response != null)
+            {
+                response.Close();
+                response = null;
+            }
+        }
+    }
+
+    private void GetLanguagesForSpeakMethod(string authToken)
+    {
+        string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
+        HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+        httpWebRequest.Headers.Add("Authorization", authToken);
+        WebResponse response = null;
+        try
+        {
+            response = httpWebRequest.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                speakLanguages = (List<string>)dcs.ReadObject(stream);
+            }
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            if (response != null)
+            {
+                response.Close();
+                response = null;
+            }
+        }
+    }
+
+    protected void CompleteButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("https://ufl.qualtrics.com/jfe/form/SV_ahh3w7Ys2rKBoTr");
+    }
+
+    protected void ErrorButton_Click(object sender, EventArgs e)
+    {
+        errorCount++;
+        ErrorLabel.Visible = true;
+        ErrorCountLabel.Text = "Error Count: " + errorCount;
+        ErrorCountLabel.Visible = true;
+    }
+
+}
